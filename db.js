848b23b973e021bd6d684e5ab598439aee94bb68
Karl Bohn <karl@McBain>
2024-05-31 20:44:25 -0400

    Routes added, search feature added

---
+++ db.js
@@ -0,0 +1,14 @@
+/** Database for lunchly */
+
+const { Pool } = require("pg");
+const dotenv = require("dotenv");
+
+const environment = process.env.NODE_ENV || "development";
+const envFile = environment === "test" ? ".env.test" : ".env";
+dotenv.config({ path: envFile });
+
+const pool = new Pool();
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+};
